1f485a8124994a6c1ee2f8f2fc23673d1a684845
mohammad-ishtiaque <mohammad.ishtiaque833@gmail.com>
2025-07-11 11:02:47 +0600

    slight change for deploy

--- next.config.js
+++ next.config.js
@@ -7,7 +7,7 @@ const nextConfig = {
     ignoreBuildErrors: true,
   },
   images: {
-    domains: ["placeholder.svg"],
+    domains: ["placeholder.svg", "i.ibb.co"],
     unoptimized: true,
   },
 }
